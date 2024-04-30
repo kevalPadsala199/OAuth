d3838ac57bb853ac6b7c4ff8fbb34793100d7744
keval_padsala <padsalakeval78@gmail.com>
2024-04-30 18:57:55 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,21 +1,20 @@
-import { Timestamp } from 'bson'
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.schema({
-    username:{
+const userSchema = new mongoose.Schema({
+    username: {
         type:String,
         required: true,
-        uniqe: true
+        unique: true,
     },
     email:{
         type:String,
         required: true,
-        uniqe: true
+        unique: true,
     },
     password:{
         type:String,
         required: true,
-    }
+    },
 }, {timestamps:true});
 
 const User = mongoose.model('User',userSchema);
